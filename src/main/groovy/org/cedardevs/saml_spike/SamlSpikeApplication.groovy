1092c76a774303d0ed367094503341f37c47a760
Bao Nguyen <nguyenbv@ciresits-MacBook-Pro-4.local>
2018-04-13 10:02:11 -0600

    I'm Batman! (This commit has no parents.)

---
+++ src/main/groovy/org/cedardevs/saml_spike/SamlSpikeApplication.groovy
@@ -0,0 +1,12 @@
+package org.cedardevs.saml_spike
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SamlSpikeApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SamlSpikeApplication, args
+	}
+}
---
+++ src/test/groovy/org/cedardevs/saml_spike/SamlSpikeApplicationTests.groovy
@@ -0,0 +1,16 @@
+package org.cedardevs.saml_spike
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class SamlSpikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
